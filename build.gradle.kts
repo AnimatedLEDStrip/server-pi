bc8dc3801937233e9530e4819721f9d351a48d62
Max Narvaez <mnarvaez@gustavus.edu>
2024-05-28 13:11:26 -0500

    Update dependencies and add release workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 tasks.wrapper {
-    gradleVersion = "7.4.2"
+    gradleVersion = "8.7"
 }
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    kotlin("jvm") version "1.9.23"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
@@ -20,17 +19,14 @@ version = "1.0"
 description = "An AnimatedLEDStrip server for Raspberry Pis"
 
 val animatedledstripServerVersion: String? by project
-val animatedledstripPiVersion: String? by project
-val alsServerVersion = animatedledstripServerVersion ?: "1.2.0-SNAPSHOT"
-//val alsPiVersion = animatedledstripPiVersion ?: "1.0.2-SNAPSHOT"
+val alsServerVersion = animatedledstripServerVersion ?: "1.1.2"
 
 sourceSets.main {
     dependencies {
-//        implementation("io.github.animatedledstrip:animatedledstrip-pi:$alsPiVersion")
         implementation("io.github.animatedledstrip:animatedledstrip-server-jvm:$alsServerVersion")
         implementation("com.github.mbelling:rpi-ws281x-java:2.0.0-SNAPSHOT")
-        api("org.apache.logging.log4j:log4j-core:2.13.2")
-        api("org.apache.logging.log4j:log4j-api:2.13.2")
+        api("org.apache.logging.log4j:log4j-core:2.23.1")
+        api("org.apache.logging.log4j:log4j-api:2.23.1")
     }
 
     java.srcDirs("src/main/kotlin")
@@ -44,11 +40,7 @@ tasks.withType<ShadowJar> {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+
+kotlin {
+    jvmToolchain(8)
 }
